5e2330c74be4e1792c7fbce619f46b1d7cfbf400
JairoRibera <jairosalvatierra098@gmail.com>
2025-10-02 08:34:48 +0200

    InicioProyecto

---
+++ Online_Killer/Assets/Code/Shoot.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Shoot : MonoBehaviour
+{
+    private Camera cam;
+    public float maxDistance = 10f;
+    public LayerMask Enemy;
+    public Text textScore;
+    private Score _scoreRef;
+    private float puntuacion = 0;
+
+    [Header("Balas y Recarga")]
+    public int cargador = 5;
+    public int bullet = 5;
+    public bool canShoot = true;
+    public bool isreloading = false;
+    private float reloadTiempo = 5f;
+    public float reloadContador = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        cargador = bullet;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Disparo();
+        Mostrartexto();
+        if (bullet <= 0)
+        {
+            InicioRecargar();
+        }
+
+        if ((isreloading == true))
+        {
+            reloadContador -= Time.deltaTime;
+            Debug.Log("Restando");
+            if (reloadContador <= 0f)
+            {
+                Debug.Log("Ya tengo balas :)");
+                isreloading = false;
+                bullet = cargador;
+                canShoot = true;
+            }
+            //return;
+        }
+        //Recargar();
+    }
+    private void Disparo()
+    {
+
+        if (canShoot == true && Input.touchCount > 0)
+        {
+            //Si el contador Touch es mayor a 1
+                //Guarda el input del primer dedo
+                Touch touch = Input.GetTouch(0);
+                //Restamos 1 a la municion
+                //Empezamos la fase Began
+                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                {
+                        bullet--;
+                    //Lanzamos un ray desde la dirección del dedo
+                    Ray ray = cam.ScreenPointToRay(Input.GetTouch(0).position);
+                    RaycastHit hit;
+                    //Si el rayo choca con un objeto con la layer enemy
+                    if (Physics.Raycast(ray, out hit, 10, Enemy))
+                    {
+                        //Obtenemos el Script Score y desactivamos el objeto
+                        _scoreRef = hit.collider.gameObject.GetComponent<Score>();
+                        hit.collider.gameObject.SetActive(false);
+                        //SI el enemigo tiene el Script le sumamos el numero a la puntuacion
+                        if (_scoreRef != null)
+                        {
+                            puntuacion = puntuacion + _scoreRef.score;
+                        }
+                        else return;
+                    }
+
+                }
+
+        }
+
+    }
+    public void InicioRecargar()
+    {
+        //if (bullet <= 0)
+        //{
+            canShoot = false;
+            isreloading = true;
+            reloadContador = reloadTiempo;
+        //}
+    }
+    //public void Recargar()
+    //{
+
+    //    InicioRecargar();
+        
+    //}
+    private void Mostrartexto()
+    {
+        //El texto será igual a la puntuacion
+        textScore.text = puntuacion.ToString();
+
+    }
+}
